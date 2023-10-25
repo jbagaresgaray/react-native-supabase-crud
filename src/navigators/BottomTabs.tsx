442f02d46c2a00b7bce371c76ebf9bb6ea7e004a
Philip Cesar Garay <philipgaray2@gmail.com>
2023-10-25 23:31:22 +0800

    Update

--- src/navigators/BottomTabs.tsx
+++ src/navigators/BottomTabs.tsx
@@ -2,6 +2,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React, { useEffect } from "react";
 
 import TabBar from "../components/TabBar";
+import TabBarButton from "../components/TabBarButton";
 import useGetAllAuthors from "../hooks/useGetAllAuthors";
 import EmptyScreen from "../screens/EmptyScreen";
 import ListScreen from "../screens/ListScreen";
@@ -18,16 +19,45 @@ const BottomTabs = () => {
 
   return (
     <BottomTab.Navigator
-      tabBar={(props) => <TabBar {...props} />}
+      // tabBar={(props) => <TabBar {...props} />}
+      safeAreaInsets={{
+        bottom: 0,
+      }}
       screenOptions={{
         tabBarIconStyle: { display: "none" },
         tabBarLabelPosition: "beside-icon",
         headerShown: false,
+        tabBarStyle: {
+          height: 70,
+          borderTopWidth: 0,
+          backgroundColor: "#ffc245",
+          paddingBottom: 20,
+          // borderBottomColor: "#ffc245",
+          // borderBottomWidth: 20,
+        },
       }}
     >
-      <BottomTab.Screen name="First" component={MainScreen} />
-      <BottomTab.Screen name="Second" component={ListScreen} />
-      <BottomTab.Screen name="Third" component={EmptyScreen} />
+      <BottomTab.Screen
+        name="First"
+        options={{
+          tabBarButton: (props) => <TabBarButton {...props} label="First" />,
+        }}
+        component={MainScreen}
+      />
+      <BottomTab.Screen
+        name="Second"
+        options={{
+          tabBarButton: (props) => <TabBarButton {...props} label="Second" />,
+        }}
+        component={ListScreen}
+      />
+      <BottomTab.Screen
+        name="Third"
+        options={{
+          tabBarButton: (props) => <TabBarButton {...props} label="Third" />,
+        }}
+        component={EmptyScreen}
+      />
     </BottomTab.Navigator>
   );
 };
